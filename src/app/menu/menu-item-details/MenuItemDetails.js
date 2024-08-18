ce26909235fa5c12783a2f9e59200faf80a8e60c
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-08-18 12:28:03 +0200

    MenuItemDetails will now always scroll to the top at mount

--- src/app/menu/menu-item-details/MenuItemDetails.js
+++ src/app/menu/menu-item-details/MenuItemDetails.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {PriceFooter} from "./PriceFooter";
 import {ImageSection} from "./ImageSection";
 import {DataSection} from "./DataSection";
 
 export const MenuItemDetails = () => {
+
+    useEffect(() => {
+        window.scrollTo({ top: 0});
+    }, []);
 
     return (
         <div className={'menu-item-details-container'}>
